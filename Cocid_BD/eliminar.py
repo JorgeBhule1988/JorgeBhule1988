201bb62284035f430665fee2bb9cc6999b71bcc5
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-03-09 23:29:48 -0700

    Update eliminar.py

--- Cocid_BD/eliminar.py
+++ Cocid_BD/eliminar.py
@@ -1,32 +1,25 @@
 import mysql.connector
 
-try:
+def eliminacion():
     connection = mysql.connector.connect(host='proweb-corp.com',
                                          database='prowebco_cocid',
                                          user='prowebco_alumnos_cocid',
                                          password='qpF5n,QDotkU')
     cursor = connection.cursor()
-    print("La tabla antes de ser eliminado el registro")
-    sql_select_query = """select * from jorge_serna_persona where id = 3"""
+    print("Registro a eliminar")
+    sql_select_query = """select * from jorge_serna_persona"""
     cursor.execute(sql_select_query)
-    record = cursor.fetchone()
-    print(record)
+    record = cursor.fetchall()
+    for row in record:
+        print(row[0], row[1], row[2], row[3], row[4], row[5])
 
     # eliminar registro
-    sql_Delete_query = """Delete from jorge_serna_persona where id = 3"""
+    borrar = int(input('Que ID deseas eliminar: '))
+    sql_Delete_query = """Delete from jorge_serna_persona where id = {}""".format(borrar)
     cursor.execute(sql_Delete_query)
     connection.commit()
     print('numero de registro eliminado', cursor.rowcount)
 
-    # verificando que la eliminacion se haya echo (es opcional)
-    cursor.execute(sql_select_query)
-    records = cursor.fetchall()
-    if len(records) == 0:
-        print("Registro eliminado satisfactoriamente ")
-
-except mysql.connector.Error as error:
-    print("Fallor la eliminacion de registro de la tabla: {}".format(error))
-finally:
     if connection.is_connected():
         cursor.close()
         connection.close()
