090eec42aed9c4710f2ec963bf254b60235b57cb
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-03-09 23:31:39 -0700

    Update insertar_clases.py

--- Cocid_BD/insertar_clases.py
+++ Cocid_BD/insertar_clases.py
@@ -1,30 +1,38 @@
 import mysql.connector
+from Persona import Persona
 
-try:
-    connection = mysql.connector.connect(host='proweb-corp.com',
-                                         database='prowebco_cocid',
-                                         user='prowebco_alumnos_cocid',
-                                         password='qpF5n,QDotkU')
+
+class Insertar:
+
+    def __init__(self):
+
+        self.connection = mysql.connector.connect(host='proweb-corp.com',
+                                            database='prowebco_cocid',
+                                            user='prowebco_alumnos_cocid',
+                                            password='qpF5n,QDotkU')
+        self.cursor = self.connection.cursor()
     
-    sql_select_Query = "select * from jorge_serna_persona"
-    cursor = connection.cursor()
-    cursor.execute(sql_select_Query)
-    # busqueda de todos los registros
-    records = cursor.fetchall()
-    print("Total de numero de registros en la tabla: ", cursor.rowcount)
-    print()
-    print("Imprimiendo cada registro ")
-    for row in records:
-        print("ID = ", row[0])
-        print("CURP = ", row[1])
-        print("NOMBRE = ", row[2])
-        print("EDAD = ", row[3])
-        print("CORREO = ", row[4])
-        print("TELEFONO = ", row[5])
-except mysql.connector.Error as error:
-    print(f"Error al leer los datos en la base de datos: {error}")
-finally:
-    if connection.is_connected():
-        cursor.close()
-        connection.close()
-        print("MySQL Conexion Cerrada")
+    def agregra(self):
+
+
+        id = int(input('dame el ID: '))
+        curp = input('Dame la CURP: ')
+        nombre = input('Dame el nombre: ')
+        edad = int(input('Dame la edad: '))
+        correo = input('Dame un correo: ')
+        telefono = int(input('Dame un numero telefonico: '))
+        Persona(curp, nombre, edad, correo, telefono)
+        usuario = Persona(curp, nombre, edad, correo, telefono)                                    
+    
+        mySql_insert_query = """INSERT INTO jorge_serna_persona (
+                                id, curp, nombre, edad, correo, telefono)
+                                VALUES ('{}', '{}', '{}', '{}', '{}', '{}')
+                                """.format(id, usuario.curp, usuario.nombre, usuario.edad, usuario.correo, usuario.telefono)
+    
+        self.cursor = self.connection.cursor()
+        self.cursor.execute(mySql_insert_query)
+        self.connection.commit()
+        print(self.cursor.rowcount, "Fue registrado satisfactoriamente ")
+        self.cursor.close()
+
+agregar = Insertar()
