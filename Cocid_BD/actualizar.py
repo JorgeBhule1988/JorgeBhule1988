72eb29edb85a0820ac239a167009dafb68b725e2
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-03-09 23:29:11 -0700

    Update actualizar.py

--- Cocid_BD/actualizar.py
+++ Cocid_BD/actualizar.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
-try:
+def actualizacion():
     connection = mysql.connector.connect(host='proweb-corp.com',
                                          database='prowebco_cocid',
                                          user='prowebco_alumnos_cocid',
@@ -8,25 +8,21 @@ try:
     cursor = connection.cursor()
 
     print("Antes de actualizar el registro ")
-    sql_select_query = """select * from jorge_serna_persona where id = 3"""
+    seleccion = int(input('Que ID desea Actualizar: '))
+    sql_select_query = """select * from jorge_serna_persona where id = {} """.format(seleccion)
     cursor.execute(sql_select_query)
     record = cursor.fetchone()
     print(record)
 
-    # actualizacion de la fila 
-    sql_update_query = """Update jorge_serna_persona set correo = 'monago_09@gmail.com' where id = 3"""
+    # actualizacion de la fila
+    campo = input('Que campo desea Actualizar: curp, nombre, edad, correo, telefono: ')
+    cambio = input('Escribe en nuevo dato: ') 
+    sql_update_query = """Update jorge_serna_persona set {} = '{}' where id = {}""".format(campo, cambio, seleccion)
     cursor.execute(sql_update_query)
     connection.commit()
     print("Actualizacion de registro satisfactorio")
 
-    print("Despues de la actualizacion de registro")
-    cursor.execute(sql_select_query)
-    record = cursor.fetchone()
-    print(record)
-
-except mysql.connector.Error as error:
-    print("Fallo la actualizacion del registro: {}".format(error))
-finally:
     if connection.is_connected():
+        cursor.close()
         connection.close()
-        print("MySQL conexion cerrada")
+        print("MySQL cconexion esta cerrada")
