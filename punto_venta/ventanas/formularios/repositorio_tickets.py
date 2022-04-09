0934b99a491db221ed3fae5c1a7a5f49624e31a9
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-04-09 10:19:12 -0600

    Update repositorio_tickets.py

--- punto_venta/ventanas/formularios/repositorio_tickets.py
+++ punto_venta/ventanas/formularios/repositorio_tickets.py
@@ -1,12 +1,13 @@
 import mysql.connector
 from formularios.repositorio_conexion import RepositorioConexionSQLite
-from formularios.tickets import Tickets
+from formularios.tickets import Tickets, Tickets2
 
 class RepositorioTickets(RepositorioConexionSQLite):
 
     def __init__(self) -> None:
         super().__init__()
-        
+
+
     def contruir_tabla(self):
         error = 0
         try:
@@ -14,7 +15,7 @@ class RepositorioTickets(RepositorioConexionSQLite):
             self.cur = self.connection.cursor()
             sql = """            
                 CREATE TABLE tabla_ticktes (
-                folio int(1111111) DEFAULT NULL,
+                numero int(1111111) DEFAULT NULL,
                 mesero varchar(100) DEFAULT NULL,
                 mesa varchar(100) DEFAULT NULL,
                 fecha varchar(100) DEFAULT NULL,
@@ -33,6 +34,61 @@ class RepositorioTickets(RepositorioConexionSQLite):
             print(f"Fallo la creacion de la tabla en MySQL: {error}")
         return error
     
+
+    def contruir_tabla2(self):
+        error = 0
+        try:
+            super().conetarse()
+            self.cur = self.connection.cursor()
+            sql = """            
+                CREATE TABLE tabla_tickets_2 (
+                numero INTEGER AUTO INCREMENT,
+                mesero varchar(100) NOT NULL,
+                mesa varchar(100) NOT NULL,
+                fecha varchar(100) NOT NULL,
+                cantidad int(100) NOT NULL,
+                producto varchar(100) NOT NULL,
+                tipo varchar(100) NOT NULL,
+                precio_unitario int(100) NOT NULL,
+                total int(100) NOT NULL,
+                PRIMARY KEY (numero)
+                ) 
+            """
+            self.cur.execute(sql)
+            self.connection.commit()
+            print("Creacion de la tabla satisfatoriamente ")
+        except :
+            error=1
+            print(f"Fallo la creacion de la tabla en MySQL: {error}")
+        return error
+    
+
+    def contruir_tabla3(self):
+        error = 0
+        try:
+            super().conetarse()
+            self.cur = self.connection.cursor()
+            sql = """            
+                CREATE TABLE tabla_tickets_cobro (
+                numero INTEGER AUTO INCREMENT,
+                mesero varchar(100) NOT NULL,
+                mesa varchar(100) NOT NULL,
+                fecha varchar(100) NOT NULL,
+                cantidad int(100) NOT NULL,
+                producto varchar(100) NOT NULL,
+                precio_unitario int(100) NOT NULL,
+                total int(100) NOT NULL,
+                PRIMARY KEY (numero)
+                ) 
+            """
+            self.cur.execute(sql)
+            self.connection.commit()
+            print("Creacion de la tabla satisfatoriamente ")
+        except :
+            error=1
+            print(f"Fallo la creacion de la tabla en MySQL: {error}")
+        return error
+
 
     def insertar(self, tickets: Tickets) -> int:
         registros_afectado = 0
@@ -57,3 +113,75 @@ class RepositorioTickets(RepositorioConexionSQLite):
             self.cerrar_conexion()
 
         return registros_afectado
+    
+
+    def insertar2(self, tickets: Tickets2) -> int:
+        registros_afectado = 0
+        try:
+            super().conetarse()
+            cursor = self.connection.cursor()
+
+            mySql_insert_query = """INSERT INTO tabla_tickets_2
+            (numero, mesero, mesa, fecha, cantidad, producto, precio_unitario, total)
+            VALUES(NULL, ?, ?, ?, ?, ?, ?, ?);
+            """
+            registros = [(tickets.mesero, tickets.mesa, tickets.fecha, tickets.cantidad, tickets.producto, tickets.precio_unitario, tickets.total)]
+
+            cursor.executemany(mySql_insert_query, registros)
+            self.connection.commit()
+            registros_afectado = cursor.rowcount
+            cursor.close()
+
+        except mysql.connector.Error as error:
+            print(f"Fallo la insercion {error}")
+        finally:
+            self.cerrar_conexion()
+
+        return registros_afectado
+    
+
+    def insertar3(self, tickets: Tickets2) -> int:
+        registros_afectado = 0
+        try:
+            super().conetarse()
+            cursor = self.connection.cursor()
+
+            mySql_insert_query = """INSERT INTO tabla_tickets_cobro
+            (numero, mesero, mesa, fecha, cantidad, producto, precio_unitario, total)
+            VALUES(NULL, ?, ?, ?, ?, ?, ?, ?);
+            """
+            registros = [(tickets.mesero, tickets.mesa, tickets.fecha, tickets.cantidad, tickets.producto, tickets.precio_unitario, tickets.total)]
+
+            cursor.executemany(mySql_insert_query, registros)
+            self.connection.commit()
+            registros_afectado = cursor.rowcount
+            cursor.close()
+
+        except mysql.connector.Error as error:
+            print(f"Fallo la insercion {error}")
+        finally:
+            self.cerrar_conexion()
+
+        return registros_afectado
+    
+
+    def eliminarcobro(self, mesa):
+        afectado = 0
+        try:
+            super().conetarse()
+            cursor = self.connection.cursor()
+
+            mySql_eliminar_query = """DELETE FROM tabla_tickets_cobro
+            WHERE mesa = ?;
+            """            
+            eliminar = (mesa,)
+            cursor.execute(mySql_eliminar_query, eliminar)
+            self.connection.commit()
+            afectado = cursor.rowcount
+            cursor.close()
+        except mysql.connector.Error as error:
+            print(f"Fallo la eliminacion {error}")
+        finally:
+            self.cerrar_conexion()
+
+        return afectado
