8d9aa6a8b0f644f23e9ec23534a22c6e14ae70ed
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-04-09 10:17:20 -0600

    Update controlador_flujo.py

--- punto_venta/ventanas/formularios/controlador_flujo.py
+++ punto_venta/ventanas/formularios/controlador_flujo.py
@@ -1,8 +1,33 @@
 from formularios.repositorio_tickets import RepositorioTickets
 
+
 def insertar_tickets(tickets):
     return RepositorioTickets().insertar(tickets)
 
 
+def insertar_tickets2(tickets):
+    return RepositorioTickets().insertar2(tickets)
+
+
+def insertar_tickets3(tickets):
+    return RepositorioTickets().insertar3(tickets)
+
+
+#def consultar_persona():    
+#    return RepositorioTickets().consultar()
+
+
+def eliminar_tickets(mesa):    
+    return RepositorioTickets().eliminarcobro(mesa)
+
+
 def construir():    
     return RepositorioTickets().contruir_tabla()
+
+
+def construir2():    
+    return RepositorioTickets().contruir_tabla2()
+
+
+def construir3():    
+    return RepositorioTickets().contruir_tabla3()
