197e85e5baa2f532608cd9884a213142d8ce02b7
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-04-09 10:20:52 -0600

    Update tickets.py

--- punto_venta/ventanas/formularios/tickets.py
+++ punto_venta/ventanas/formularios/tickets.py
@@ -32,3 +32,49 @@ class Tickets:
         
     def __str__(self) -> str:
         return ("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(self.folio, self.mesero, self.mesa, self.fecha, self.total, self.tipopago))
+
+
+class Tickets2:
+
+    numero: int = None
+    mesero: str = None
+    mesa: str = None
+    fecha: str = None
+    cantidad: int = None
+    producto: int = None
+    precio_unitario = None
+    total: str = None
+    tipopago: str = None
+
+    
+    def __init__(self) -> None:
+        pass
+
+    
+    def asignar(self, numero, mesero, mesa, fecha, cantidad, producto, precio_unitario, total) -> None:
+        self.numero = numero
+        self.mesero = mesero
+        self.mesa = mesa
+        self.fecha = fecha
+        self.cantidad = cantidad
+        self.producto = producto
+        self.precio_unitario = precio_unitario
+        self.total = total
+        return self
+
+
+    def mostrar_info(self) -> None:
+        print("FOLIO : ", self.numero)
+        print("MESERO : ", self.mesero)
+        print("MESA : ", self.mesa)
+        print("FECHA : ", self.fecha)
+        print("CANTIDAD : ", self.cantidad)
+        print("PRODUCTO: ", self.producto)
+        print("TIPO: ", self.tipo)
+        print("PRECIO UNITARIO: ", self.precio_unitario)
+        print("TOTAL : ", self.total)
+        
+        
+    def __str__(self) -> str:
+        return ("{:<15} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15} {:<15}".format(self.numero, self.mesero, self.mesa, self.fecha, 
+                                                                self.cantidad, self.producto, self.precio_unitario, self.total))
