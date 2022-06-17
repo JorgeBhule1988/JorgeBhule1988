56df25e20818d69f53ea67c4c1aa2a77a8281b7c
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-06-16 22:09:59 -0600

    Update app.py

--- proyectofinal/presentacion/app.py
+++ proyectofinal/presentacion/app.py
@@ -10,3 +10,16 @@ class App(AppDesigner):
     def __init__(self):
         super().__init__()
         self.initialize_component()
+        self.carga_df()
+        self.cargar_tabla()
+
+
+    def carga_df(self):
+
+        self.df = pd.read_csv(PATH_FILE)
+        self.df = self.df.replace(' ', pd.NA)
+    
+
+    def cargar_tabla(self):
+
+        self.tabladedatosdf.model.df = self.df
