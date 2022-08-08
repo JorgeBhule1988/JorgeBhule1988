36a08bb8b39c96f8ea9b11a1b0ea7dde44fe1894
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-08-07 19:58:27 -0600

    Update repositorio_tickets.py

--- punto_venta/ventanas/formularios/repositorio_tickets.py
+++ punto_venta/ventanas/formularios/repositorio_tickets.py
@@ -1,6 +1,6 @@
 import mysql.connector
-from formularios.repositorio_conexion import RepositorioConexionSQLite
-from formularios.tickets import Tickets, Tickets2
+from formularios_punto_de_venta.repositorio_conexion import RepositorioConexionSQLite
+from formularios_punto_de_venta.tickets import Tickets, Tickets2, Producto
 
 class RepositorioTickets(RepositorioConexionSQLite):
 
@@ -8,20 +8,19 @@ class RepositorioTickets(RepositorioConexionSQLite):
         super().__init__()
 
 
-    def contruir_tabla(self):
+    def contruir_tabla1(self):
         error = 0
         try:
             super().conetarse()
             self.cur = self.connection.cursor()
             sql = """            
                 CREATE TABLE tabla_venta_diaria (
-                folio INTEGER AUTOINCREMENT,
+                numero INTEGER PRIMARY KEY AUTOINCREMENT,
                 mesero varchar(100) NOT NULL,
                 mesa varchar(100) NOT NULL,
                 fecha varchar(100) NOT NULL,
                 total int(100) NOT NULL,
-                tipopago varchar(100) NOT NULL,
-                PRIMARY KEY (folio)
+                tipopago varchar(100) NOT NULL
                 ) 
             """
             self.cur.execute(sql)
@@ -40,15 +39,14 @@ class RepositorioTickets(RepositorioConexionSQLite):
             self.cur = self.connection.cursor()
             sql = """            
                 CREATE TABLE tabla_tickets_principal (
-                numero INTEGER AUTOINCREMENT,
+                numero INTEGER PRIMARY KEY AUTOINCREMENT,
                 mesero varchar(100) NOT NULL,
                 mesa varchar(100) NOT NULL,
                 fecha varchar(100) NOT NULL,
                 cantidad int(100) NOT NULL,
                 producto varchar(100) NOT NULL,
                 precio_unitario int(100) NOT NULL,
-                total int(100) NOT NULL,
-                PRIMARY KEY (numero)
+                total int(100) NOT NULL
                 ) 
             """
             self.cur.execute(sql)
@@ -67,15 +65,38 @@ class RepositorioTickets(RepositorioConexionSQLite):
             self.cur = self.connection.cursor()
             sql = """            
                 CREATE TABLE tabla_cobro (
-                numero INTEGER AUTOINCREMENT,
+                numero INTEGER PRIMARY KEY AUTOINCREMENT,
                 mesero varchar(100) NOT NULL,
                 mesa varchar(100) NOT NULL,
                 fecha varchar(100) NOT NULL,
                 cantidad int(100) NOT NULL,
                 producto varchar(100) NOT NULL,
                 precio_unitario int(100) NOT NULL,
-                total int(100) NOT NULL,
-                PRIMARY KEY (numero)
+                total int(100) NOT NULL
+                ) 
+            """
+            self.cur.execute(sql)
+            self.connection.commit()
+            print("Creacion de la tabla satisfatoriamente ")
+        except :
+            error=1
+            print(f"Fallo la creacion de la tabla en MySQL: {error}")
+        return error
+
+    
+    def construir_tabla4(self):
+        error = 0
+        try:
+            super().conetarse()
+            self.cur = self.connection.cursor()
+            sql = """            
+                CREATE TABLE tabla_producto (
+                id_producto INTEGER PRIMARY KEY AUTOINCREMENT,
+                nombre varchar(100) NOT NULL,
+                categoria varchar(100) NOT NULL,
+                precio_unitario int(100) NOT NULL,
+                stock int(100) NOT NULL,
+                existencia varchar(100) NOT NULL
                 ) 
             """
             self.cur.execute(sql)
@@ -94,7 +115,7 @@ class RepositorioTickets(RepositorioConexionSQLite):
             cursor = self.connection.cursor()
 
             mySql_insert_query = """INSERT INTO tabla_venta_diaria
-            (folio, mesero, mesa, fecha, total, tipopago)
+            (numero, mesero, mesa, fecha, total, tipopago)
             VALUES(NULL, ?, ?, ?, ?, ?);
             """
             registros = (tickets.mesero, tickets.mesa, tickets.fecha, tickets.total, tickets.tipopago)
@@ -161,6 +182,30 @@ class RepositorioTickets(RepositorioConexionSQLite):
 
         return registros_afectado
     
+
+    def insertar4(self, producto: Producto) -> int:
+        registros_afectado = 0
+        try:
+            super().conetarse()
+            cursor = self.connection.cursor()
+
+            mySql_insert_query = """INSERT INTO tabla_producto
+            (id_producto, nombre, categoria, precio_unitario, stock, existencia)
+            VALUES(NULL, ?, ?, ?, ?, ?);
+            """
+            registros = (producto.id_producto, producto.nombre, producto.categoria, producto.precio_unitario, producto.stock, producto.existencia)
+
+            cursor.execute(mySql_insert_query, registros)
+            self.connection.commit()
+            registros_afectado = cursor.rowcount
+            cursor.close()
+
+        except mysql.connector.Error as error:
+            print(f"Fallo la insercion {error}")
+        finally:
+            self.cerrar_conexion()
+
+        return registros_afectado
 
     def eliminarcobro(self, mesa):
         afectado = 0
