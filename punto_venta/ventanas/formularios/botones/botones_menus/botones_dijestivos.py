144be5f7936d9a62c4b6278cb5cfec0d55240504
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-08-08 19:18:51 -0600

    Create botones_dijestivos.py

---
+++ punto_venta/ventanas/formularios/botones/botones_menus/botones_dijestivos.py
@@ -0,0 +1,117 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+class BotonesDojestivos:
+
+    def kahlua1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Kahlua', '120', subtotal))
+            self.ecantidad.delete(0, END)
+    
+
+    def licor1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Licor 43', '120', subtotal))
+            self.ecantidad.delete(0, END)
+
+
+    def baileys1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Baileys', '120', subtotal))
+            self.ecantidad.delete(0, END)
+    
+
+    def disarono1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Disarono', '120', subtotal))
+            self.ecantidad.delete(0, END)
+
+    
+    def zambuca1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Zambuca', '120', subtotal))
+            self.ecantidad.delete(0, END)    
+    
+
+    def zambucanegro1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Zambuca Negro', '120', subtotal))
+            self.ecantidad.delete(0, END)
+
+
+    def marnier1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 160
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Grand Marnier', '160', subtotal))
+            self.ecantidad.delete(0, END)
+
+
+    def frangelico1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 120
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Frangelico', '120', subtotal))
+            self.ecantidad.delete(0, END)
+
+
+    def courvusier1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 180
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Courvusier VS', '180', subtotal))
+            self.ecantidad.delete(0, END)
+
+    
+    def azteca1(self):
+
+        if(len(self.emesero.get()) == 0) or (len(self.emesa.get()) == 0) or (len(self.ecantidad.get()) == 0):
+            messagebox.showwarning(message = 'Falta seleccionar el mesero o el numero de mesa o la cantidad', title = 'Warning')
+        else:
+            subtotal = float(self.ecantidad.get()) * 100
+            self.captura.insert('', END, text = str(self.ecantidad.get()), values = ('Azteca de Oro', '100', subtotal))
+            self.ecantidad.delete(0, END)
+
+
+    def __init__(self, v):
+
+            cantidad = IntVar()
+            lista_mesas = ['1', '3', '5', '6', '7', '8', '9', '10', '11', '12', '13', 'Cava1', 'Cava2', 'Cava3', 'Cava4']
+            lista_meseros = ['ERICK MARTINEZ', 'DIEGO HERNANDEZ', 'MANUEL FLORES', 'MIGUEL CORONA']
+
+            self.captura = ttk.Treeview(v, columns=('#0', '#1', '#2'))
+            self.ecantidad = Entry(v, textvariable = cantidad)
+            self.emesa = ttk.Combobox(v, value = lista_mesas)
+            self.emesero = ttk.Combobox(v, value = lista_meseros)
