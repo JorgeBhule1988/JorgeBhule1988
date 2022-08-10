6db570075f55691349ee0fc662f012458d0073e7
JorgeBhule1988 <100169879+JorgeBhule1988@users.noreply.github.com>
2022-08-10 10:44:38 -0600

    Update controlador_flujo.py

--- punto_venta/dominio/controlador_flujo.py
+++ punto_venta/dominio/controlador_flujo.py
@@ -1,4 +1,4 @@
-from formularios.repositorio_tickets import RepositorioTickets
+from persistencia.repositorio_tickets import RepositorioTickets
 
 
 def insertar_tickets(tickets):
@@ -7,6 +7,10 @@ def insertar_tickets(tickets):
 
 def insertar_tickets2(tickets):
     return RepositorioTickets().insertar2(tickets)
+
+
+def insertar_producto(producto):
+    return RepositorioTickets().insertar4(producto)
 
 
 def insertar_tickets3(tickets):
@@ -20,16 +24,17 @@ def insertar_tickets3(tickets):
 def eliminar_tickets(mesa):    
     return RepositorioTickets().eliminarcobro(mesa)
 
-def eliminar_fila_cobro(producto):
+
+def eliminar_fila_cobro(producto):    
     return RepositorioTickets().eliminarproducto(producto)
 
 
-def eliminar_fila_cobro2(producto):
-    return RepositorioTickets().eliminarproducto2(producto)    
+def eliminar_fila_cobro2(producto):    
+    return RepositorioTickets().eliminarproducto2(producto)
 
 
-def construir():    
-    return RepositorioTickets().contruir_tabla()
+def construir1():    
+    return RepositorioTickets().contruir_tabla1()
 
 
 def construir2():    
@@ -38,3 +43,11 @@ def construir2():
 
 def construir3():    
     return RepositorioTickets().contruir_tabla3()
+
+
+def construir4():
+    return RepositorioTickets().construir_tabla4()
+
+
+def consultar1():
+    return RepositorioTickets().consultarproducto()
